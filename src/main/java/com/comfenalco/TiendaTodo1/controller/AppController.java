ac1f516e2d30c495cd5ce8b2389e2a1922abae24
penas <penas@LAPTOP-AIBTRB4O>
2020-05-03 17:35:48 -0500

    Creación de rama develop

--- src/main/java/com/comfenalco/TiendaTodo1/controller/AppController.java
+++ src/main/java/com/comfenalco/TiendaTodo1/controller/AppController.java
@@ -24,6 +24,7 @@ import com.comfenalco.TiendaTodo1.service.IProductoService;
 @SessionAttributes("producto")
 public class AppController {
 	
+	//comment
 	@Autowired
 	private IProductoService productoService;
 		
